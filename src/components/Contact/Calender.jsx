18efbe1ded9f16fe780126ff5b6a23246624388c
Rizwan Hanif <rizwanhanif633@gmail.com>
2025-03-05 19:53:15 +0500

    minor change

--- src/components/Contact/Calender.jsx
+++ src/components/Contact/Calender.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { InlineWidget } from "react-calendly";
 const Calendar = () => {
+  const calendlyUrl = 'https://calendly.com/zenaptic-labs/30min'; // will replace with env variable later
   return (
     <>
       <InlineWidget
-        url="https://calendly.com/zenaptic-labs/30min"
+        url={calendlyUrl}
         pageSettings={{
           backgroundColor: "#fff",
         }}
