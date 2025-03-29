7365432f86d5e74fe2bbdfa0915dcd82b4b970dc
Sung-Hyun Kang <sunghyunkang@microsoft.com>
2025-03-29 12:35:35 -0500

    Added everything

--- src/components/Home/Testimonials.jsx
+++ src/components/Home/Testimonials.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
+import VideoPlayer from "./VideoPlayer";
 
 const testimonials = [
   {
@@ -47,28 +48,6 @@ const testimonials = [
   },
 ];
 
-const playVideo = (index) => {
-  const video = document.getElementById(`video-${index}`);
-  const overlay = document.getElementById(`overlay-${index}`);
-  const allVideos = document.querySelectorAll("video");
-  const allOverlays = document.querySelectorAll("[id^=overlay-]");
-
-  allVideos.forEach((vid, idx) => {
-    if (idx !== index) {
-      vid.pause();
-      allOverlays[idx].style.display = "flex";
-    }
-  });
-
-  if (video.paused) {
-    video.play();
-    overlay.style.display = "none";
-  } else {
-    video.pause();
-    overlay.style.display = "flex";
-  }
-};
-
 const Testimonials = () => {
   return (
     <div className="w-[100vw] justify-center items-center flex">
@@ -84,7 +63,7 @@ const Testimonials = () => {
                 type: "slide",
                 gap: "1rem",
                 height: "auto",
-                arrows: true,
+                arrows: false,
                 perMove: 5,
                 focus: "center",
                 pagination: false,
@@ -106,25 +85,11 @@ const Testimonials = () => {
                   key={index}
                   onClick={() => playVideo(index)}
                 >
-                  <div className="relative w-full h-auto rounded-xl    overflow-hidden">
-                    <video id={`video-${index}`} className="w-full ">
-                      <source src={testimonial.video_url} type="video/mp4" />
-                      Your browser does not support the video tag.
-                    </video>
-                    <div id={`overlay-${index}`} className="overlay">
-                      <img
-                        src="./assets/play-opacity.svg"
-                        alt="logo"
-                        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                      />
-                      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-end items-end bg-gradient-to-b from-transparent to-black text-white text-base p-4 leading-normal">
-                        <p className="mb-2 relative after:content-[url('../../public/assets/square-quot.svg')] after:absolute after:left-[-20px] after:top-[-40px] max-lg:text-[16px] max-lg:font-normal">
-                          {testimonial.description}
-                        </p>
-                        <span>{testimonial.nameOFInfluecer}</span>
-                      </div>
-                    </div>
-                  </div>
+                  <VideoPlayer
+                    videoUrl={testimonial.video_url}
+                    description={testimonial.description}
+                    influencerName={testimonial.nameOFInfluecer}
+                  />
                 </SplideSlide>
               ))}
             </Splide>
